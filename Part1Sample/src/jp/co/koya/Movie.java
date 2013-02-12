abcc72cae1174b9c1bf46256aea8a07d82f54b11
Noriaki Koyanagi <koyanagn@gmail.com>
2013-02-13 06:22:54 +0900

    First Commit

---
+++ Part1Sample/src/jp/co/koya/Movie.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package jp.co.koya;
+
+/**
+ * @author k_nori-yagi
+ *
+ */
+public class Movie {
+	public static final int CHILDRENS = 2;
+	public static final int REGULAR = 0;
+	public static final int NEW_RELEASE = 1;
+	
+	private String _title;
+	private int _priceCode;
+	
+	public Movie(String title, int priceCode) {
+		_title = title;
+		_priceCode = priceCode;
+	}
+
+	/**
+	 * @return _priceCode
+	 */
+	public int getPriceCode() {
+		return _priceCode;
+	}
+
+	/**
+	 * @param _priceCode ƒZƒbƒg‚·‚é _priceCode
+	 */
+	public void setPriceCode(int arg) {
+		this._priceCode = arg;
+	}
+
+	/**
+	 * @return _title
+	 */
+	public String getTitle() {
+		return _title;
+	}
+
+}
